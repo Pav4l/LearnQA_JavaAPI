912f9c9b8398ffdbb72c9750fc8dd491a0c60276
Pavel Gorev <pavel.gorev@gmail.com>
2024-06-10 21:48:15 +0300

    05. Парсинг JSONГиперссылка

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,15 +1,29 @@
 import io.restassured.RestAssured;
-import io.restassured.response.Response;
+import io.restassured.path.json.JsonPath;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class HelloWorldTest {
 
     @Test
-    public void testHelloWorld(){
-        Response response = RestAssured
-                .get("https://playground.learnqa.ru/api/get_text")
-                .andReturn();
-        response.prettyPrint();
+    public void testResAssured(){
+        Map<String, String> params = new HashMap<>();
+        params.put("name","John");
+
+        JsonPath response = RestAssured
+                .given()
+                .queryParams(params)
+                .get("https://playground.learnqa.ru/api/hello")
+                .jsonPath();
+
+        String name = response.get("answer2");
+        if (name == null){
+            System.out.println("The key 'answer2' is absent");
+        } else {
+            System.out.println(name);
+        }
     }
 }
