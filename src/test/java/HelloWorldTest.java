8835bd235701942a058acea8d648c4615646485c
Pavel Gorev <pavel.gorev@gmail.com>
2024-06-11 14:35:00 +0300

    07. Коды ответа сервера

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,6 +1,5 @@
 import io.restassured.RestAssured;
-import io.restassured.path.json.JsonPath;
-
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -10,20 +9,16 @@ public class HelloWorldTest {
 
     @Test
     public void testResAssured(){
-        Map<String, String> params = new HashMap<>();
-        params.put("name","John");
 
-        JsonPath response = RestAssured
+        Response response = RestAssured
                 .given()
-                .queryParams(params)
-                .get("https://playground.learnqa.ru/api/hello")
-                .jsonPath();
+                .redirects()
+                .follow(true)
+                .post("https://playground.learnqa.ru/api/get_303")
+                .andReturn();
 
-        String name = response.get("answer2");
-        if (name == null){
-            System.out.println("The key 'answer2' is absent");
-        } else {
-            System.out.println(name);
-        }
+        int statusCode = response.getStatusCode();
+        System.out.println(statusCode);
+        response.print();
     }
 }
