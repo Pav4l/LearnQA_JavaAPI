fb0a970b131a962b32904fc999244c4f1c593073
Pavel Gorev <pavel.gorev@gmail.com>
2024-06-11 17:25:44 +0300

    08. Заголовки

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,4 +1,5 @@
 import io.restassured.RestAssured;
+import io.restassured.http.Headers;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
@@ -9,16 +10,26 @@ public class HelloWorldTest {
 
     @Test
     public void testResAssured(){
+        Map<String,String > headers = new HashMap<>();
+        headers.put("myHeader1", "myValue1");
+        headers.put("myHeader2", "myValue");
 
         Response response = RestAssured
                 .given()
+                //.headers(headers)
                 .redirects()
-                .follow(true)
-                .post("https://playground.learnqa.ru/api/get_303")
+                .follow(false)
+                .when()
+                //.get("https://playground.learnqa.ru/api/show_all_headers")
+                .get("https://playground.learnqa.ru/api/get_303")
                 .andReturn();
 
-        int statusCode = response.getStatusCode();
-        System.out.println(statusCode);
-        response.print();
+        response.prettyPrint();
+
+        //Headers responseHeaders = response.getHeaders();
+        String locationHeader = response.getHeader("Location");
+        System.out.println(locationHeader);
+        //System.out.println(responseHeaders);
+
     }
 }
