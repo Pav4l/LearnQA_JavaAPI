2f4c95b3c6ba468b700571acd0a77ad1e4ac272e
Pavel Gorev <pavel.gorev@gmail.com>
2024-06-13 23:00:31 +0300

    Ex7: Долгий редирект

---
+++ src/test/java/LongRedirectEx7.java
@@ -0,0 +1,34 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class LongRedirectEx7 {
+
+    @Test
+    public void testLongRedirect() {
+        String url = "https://playground.learnqa.ru/api/long_redirect";
+        int redirectCount = 0;
+        Response response;
+
+        do {
+            response = RestAssured
+                    .given()
+                    .redirects()
+                    .follow(false)
+                    .get(url)
+                    .andReturn();
+
+            String locationHeader = response.getHeader("Location");
+            if (locationHeader != null) {
+                url = locationHeader;
+                redirectCount++;
+                System.out.println("Redirect to: " + url);
+            } else {
+                break;
+            }
+        } while (response.getStatusCode() != 200);
+
+        System.out.println("Final URL: " + url);
+        System.out.println("Number of redirects: " + redirectCount);
+    }
+}
