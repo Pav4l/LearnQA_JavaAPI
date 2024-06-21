361510b376cf1448f99ff8f3ba569414f5f9d709
Pavel Gorev <pavel.gorev@gmail.com>
2024-06-21 23:02:52 +0300

    Лекция 4.1 Создание существующего пользователя

--- src/test/java/lib/Assertions.java
+++ src/test/java/lib/Assertions.java
@@ -1,6 +1,7 @@
 package lib;
 
 import io.restassured.response.Response;
+import org.apache.groovy.parser.antlr4.GroovyParser;
 
 import static org.hamcrest.Matchers.hasKey;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,5 +12,21 @@ public class Assertions {
 
         int value = Response.jsonPath().getInt(name);
         assertEquals(expectedValue, value, "JSON value is no equal to expected value");
+    }
+
+    public static void assertResponseTextEquals(Response Response, String expectedAnswer){
+        assertEquals(
+                expectedAnswer,
+                Response.asString(),
+                "Response text is not expected"
+        );
+    }
+
+    public static void assertResponseCodeEquals(Response Response, int expectedStatusCode){
+        assertEquals(
+                expectedStatusCode,
+                Response.statusCode(),
+                "Response status code is not expected"
+        );
     }
 }
