82477329b71dabe7ef2323865c3703263a7c4066
Pavel Gorev <pavel.gorev@gmail.com>
2024-06-29 22:18:51 +0300

    Лекция 4.3 Просмотр пользователя

--- src/test/java/lib/Assertions.java
+++ src/test/java/lib/Assertions.java
@@ -1,9 +1,9 @@
 package lib;
 
 import io.restassured.response.Response;
-import org.apache.groovy.parser.antlr4.GroovyParser;
 
 import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.not;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Assertions {
@@ -30,7 +30,17 @@ public class Assertions {
         );
     }
 
-    public static void assertJsonHasKey(Response Response, String ExpectedFiledName){
+    public static void assertJsonHasField(Response Response, String ExpectedFiledName){
         Response.then().assertThat().body("$", hasKey(ExpectedFiledName));
+    }
+
+    public static void assertJsonHasFields(Response Response, String[] expectedFieldNames){
+        for (String expectedFieldName : expectedFieldNames) {
+            Assertions.assertJsonHasField(Response, expectedFieldName);
+        }
+    }
+
+    public static void assertJsonHasNotField(Response Response, String unexpectedFieldName){
+        Response.then().assertThat().body("$",not(hasKey(unexpectedFieldName)));
     }
 }
